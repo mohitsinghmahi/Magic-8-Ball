3cae0bbfb4fb2fd053d6cbb64b6e5e21e8869a95
mohitsinghmahi <33360180+mohitsinghmahi@users.noreply.github.com>
2021-04-20 12:43:12 -0400

    Update and rename 8ball.py to Toronto Man 8ball.py

---
+++ Toronto Man 8ball.py
@@ -0,0 +1,41 @@
+import discord
+from discord.ext import commands
+import random
+
+client = commands.Bot(command_prefix='.')
+
+
+@client.event
+async def on_ready():
+    print('Bot is ready.')
+
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+
+
+@client.command(aliases=['8ball', 'test'])
+async def _8ball(ctx, *, question):
+    responses = ['yoo dis kids on something no kizzy',
+                 'bro you bless? why da fuck would you think thats true 😂',
+                 'Lowkey not lmao',
+                 'Bruh what drugs are you on?',
+                 'cro ur trippin',
+                 'youre a wasteyute if you think thats true' ]
+    await ctx.send(f'Question: {question} \nAnswer: {random.choice(responses)}')
+
+@client.event
+async def on_member_join(member):
+    print(f'{member} has joined a server.')
+
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} has left the server.')
+    
+@client.event
+async 
+
+
+client.run('TOKEN')
